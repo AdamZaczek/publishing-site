33ae76b1556b098c7815aec57387bb04302c5533
Adam Żaczek <adamzaczekul@gmail.com>
2016-02-17 01:07:26 +0000

    Add Asynchronous Rendering And Toggling To Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,13 @@
 class CommentsController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
-		@comment = @post.comments.create(params[:comment].permit(:content))
-		@comment.save
+		@comment = @post.comments.new(params[:comment].permit(:content))
+		@comment.save!
 
-		redirect_to post_path(@post)
+		respond_to do |format|
+			format.html { redirect_to @post }
+			format.js
+		end
 	end
 
 	def destroy
@@ -12,6 +15,6 @@ class CommentsController < ApplicationController
 		@comment = @post.comments.find(params[:id])
 		@comment.destroy
 
-		redirect_to post_path(@post)
+		redirect_to @post
 	end
 end
